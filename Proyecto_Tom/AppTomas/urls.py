7d0dbf85e7ab7b49aed223fdb8a02f83794ee622
Tomas Herrera <tomyherrera28@gmail.com>
2024-08-18 19:10:54 -0300

    En proceso

--- Proyecto_Tom/AppTomas/urls.py
+++ Proyecto_Tom/AppTomas/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from AppTomas import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.index, name="Home"),
+    path('products/', views.products, name="Products"),
+    path('contact/', views.contact, name="Contact"),
 ]
--- Proyecto_Tom/AppTomas/views.py
+++ Proyecto_Tom/AppTomas/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def inicio(request):
+def index(request):
     return render(request, "AppTomas/index.html")
+
+def products(request):
+    return render(request, "AppTomas/products.html")
+
+def contact(request):
+    return render(request, "AppTomas/contact.html")
