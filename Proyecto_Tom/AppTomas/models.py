40a5f48460579344b632b543449d99f589af6327
Tomas Herrera <tomyherrera28@gmail.com>
2024-08-18 00:32:06 -0300

    En proceso

--- Proyecto_Tom/AppTomas/models.py
+++ Proyecto_Tom/AppTomas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    
+class Cliente(models.Model):
+    user = models.CharField(max_length=40)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    
+class Carrito(models.Model):
+    nombre = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    vendido = models.BooleanField()
