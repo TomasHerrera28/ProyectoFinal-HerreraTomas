4e4ccc981b4cfcca4296d1d617d6daf90dd4df30
Tomas Herrera <tomyherrera28@gmail.com>
2024-09-05 11:45:44 -0300

    Proyecto terminado

--- Proyecto_Tom/AppTomas/models.py
+++ Proyecto_Tom/AppTomas/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.IntegerField()
     imagen = models.ImageField(upload_to='products', null=True, blank=True)
     
+    
     def __str__(self):
         return "{} | ID: {}".format(self.name, self.id)
     
