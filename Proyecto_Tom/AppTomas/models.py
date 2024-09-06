467441a50a0199565c609daaaa0d992ea243b7d0
Tomas Herrera <tomyherrera28@gmail.com>
2024-09-06 00:00:30 -0300

    Proyecto terminado

--- Proyecto_Tom/AppTomas/models.py
+++ Proyecto_Tom/AppTomas/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=40)
     price = models.IntegerField()
     imagen = models.ImageField(upload_to='products', null=True, blank=True)
-    
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
     
     def __str__(self):
         return "{} | ID: {}".format(self.name, self.id)
